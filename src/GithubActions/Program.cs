ce8f240cc9bf6d510afdaf72de45a462be7d1412
Kelly Starling <kstarling@formfree.com>
2022-06-14 10:29:04 -0400

    testing actions with change to branch name

--- src/GithubActions/Program.cs
+++ src/GithubActions/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-
 var configurations = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
